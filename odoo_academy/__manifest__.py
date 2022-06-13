19eeaaa6bbd8a45b38e0ac356ed00b8d32bc5baa
Lars Gartenberg <laga@odoo.com>
2022-06-13 17:22:23 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': 'yup',
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
